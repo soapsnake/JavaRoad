b988b3a41103c5369ad0d2496f4db93b7c0d471a
51721198 <51721198@qq.com>
2016-11-06 18:56:47 +0800

    interrupt features

---
+++ runnable/src/main/java/com/ld/runnable/interrupt/IOBlocked.java
@@ -0,0 +1,29 @@
+package com.ld.runnable.interrupt;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IOBlocked implements Runnable {  	//IO工作线程
+	
+	private InputStream in;
+	public  IOBlocked(InputStream is) {
+		in = is;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			System.out.println("waiting for read(): ");
+			in.read();      //读取输入流的数据
+		} catch (IOException e) {
+			System.out.println("线程抛出了IO异常!!!!");
+			if(Thread.currentThread().isInterrupted()){          //如果这个工作线程被打断
+				System.out.println("IO线程收到打断信号interrupted from blocked I/O"); //被I/O流打断
+			}else {
+				System.out.println("没有收到打断异常,但是收到了运行时异常");
+				throw new RuntimeException(e);      //没有打断则抛出运行时异常
+			}
+		}
+		System.out.println("1111111111111111111111111111111111111111EXITING IOblocked.run()");
+	}
+}
---
+++ runnable/src/main/java/com/ld/runnable/interrupt/SleepBlocked.java
@@ -0,0 +1,18 @@
+package com.ld.runnable.interrupt;
+
+import java.util.concurrent.TimeUnit;
+
+public class SleepBlocked implements Runnable{ //睡眠工作线程
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			TimeUnit.SECONDS.sleep(100);         //睡眠100秒
+		} catch (InterruptedException e) {
+			System.out.println("线程的睡眠状态遭遇异常!!!!!!!!!!!!!!!!!");
+		}
+		
+		System.out.println("Exiting sleepBolcked.run()");
+	}
+}
