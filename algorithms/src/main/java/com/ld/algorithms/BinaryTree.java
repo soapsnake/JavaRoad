d9b6e4644794b569ddec4a6168ec91dbd538008c
51721198@qq.com <51721198@qq.com>
2017-02-19 13:48:29 +0800

    test helper store

--- algorithms/src/main/java/com/ld/algorithms/BinaryTree.java
+++ algorithms/src/main/java/com/ld/algorithms/BinaryTree.java
@@ -1,9 +1,7 @@
 package com.ld.algorithms;
 
 public class BinaryTree<Key extends Comparable<Key>,Value>{
-	
 	private Node rootnode;
-	
 	private Node node;
 	
 	private class Node{
@@ -12,18 +10,41 @@ public class BinaryTree<Key extends Comparable<Key>,Value>{
 		private Node left;
 		private Node right;
 		private int countNumber;
+		
+		public Node() {
+			this.key = key;
+			this.value = value;
+			this.countNumber = countNumber;
+		}
 	}
-	
 	
 	public BinaryTree() {
 	this.rootnode = rootnode;
 	this.node = node;
 	}
 	
-	public int getCountNumber(){
-		//return countNumber;
+	//容量
+	public int size(){
 		return 0;
 	}
 	
-
+	//查
+	public Value getValue(Key key){
+		return null;
+	}
+	
+	//增．改
+	public void put(Key key,Value value){
+		
+	}
+	
+	//删
+	public void delete(Key key){
+		
+	}
+	
+	public int rank(){
+		return 0;
+	}
+	
 }
