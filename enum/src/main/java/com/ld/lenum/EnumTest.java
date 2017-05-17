0785dd7fd3d48a7eb5100e6d81992f9f1a5b01d2
刘盾 <liudun@liudundeMacBook-Pro.local>
2017-05-17 20:11:30 +0800

    some new topic

---
+++ enum/src/main/java/com/ld/lenum/EnumTest.java
@@ -0,0 +1,17 @@
+package com.ld.lenum;
+
+/**
+ * Created by liudun on 2017/5/16.
+ */
+public class EnumTest {
+
+    public static void main(String[] args){
+        System.out.println("start!");
+
+
+
+
+    }
+
+
+}
