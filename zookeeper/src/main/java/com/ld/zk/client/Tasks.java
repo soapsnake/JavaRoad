36d1a18a81423d6cbef73a5c707c30ed8a2079a3
刘盾 <liudun@liudundeMacBook-Pro.local>
2017-05-21 02:16:41 +0800

    zookeeper study!!!!!

---
+++ zookeeper/src/main/java/com/ld/zk/client/Tasks.java
@@ -0,0 +1,57 @@
+package com.ld.zk.client;
+
+import com.ld.zk.constant.ZkConstant;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+/**
+ * Created by liudun on 2017/5/20.
+ */
+public class Tasks implements Watcher {
+    private ZooKeeper zk;
+    private String hostport;
+
+    Tasks(String hostport){
+        this.hostport = hostport;
+    }
+
+    public void process(WatchedEvent event) {
+        System.out.println(event.toString());
+    }
+
+    private void startZK() throws IOException {
+        zk = new ZooKeeper(hostport,1500,this);
+    }
+
+    private void stopZK() throws InterruptedException {
+        zk.close();
+    }
+
+    String createTasks(String command) throws KeeperException, InterruptedException {
+        while (true){
+                String name = zk.create("/tasks/task-",
+                        command.getBytes(),
+                        ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                        CreateMode.EPHEMERAL_SEQUENTIAL);
+                return name;
+        }
+    }
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        Tasks tasks = new Tasks(ZkConstant.ZK_HOST_PORT);
+        tasks.startZK();
+
+        String command = "";
+        String name = tasks.createTasks(command);
+
+        Thread.sleep(30000);
+
+        System.out.println("task been created: "+name);
+    }
+}
