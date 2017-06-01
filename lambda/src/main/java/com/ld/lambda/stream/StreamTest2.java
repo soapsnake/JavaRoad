b11d131f9470aa10ef36119ee1363eb9c3379c85
刘盾 <liudun@liudundeMacBook-Pro.local>
2017-06-01 18:38:00 +0800

    fix

--- lambda/src/main/java/com/ld/lambda/stream/StreamTest2.java
+++ lambda/src/main/java/com/ld/lambda/stream/StreamTest2.java
@@ -10,8 +10,6 @@ public class StreamTest2 {
 
     public static void main(String[] args){
 
-
-        long[] arrs = {1,2,3,4,5,6,7,8,9,10};
         List<Person> list = new ArrayList<>();
         for (long i=0;i<101;i++){
             Person person = new Person();
@@ -21,15 +19,15 @@ public class StreamTest2 {
             list.add(person);
         }
 
-
-
         long res =  list.stream().mapToLong(Person::getId).sum();
 
         long age = list.stream().mapToLong(Person::getAge).sum();
 
-        System.out.println(res);
+        System.out.println("size: "+list.size());
 
-        System.out.println(age);
+        System.out.println("res: "+res);
+
+        System.out.println("age: "+age);
 
 //        long totalDepositAmount = resps.stream().mapToLong(TotalReceiptsResp::getTotalDepositAmount).sum();
 //        long totalCardAmount = resps.stream().mapToLong(TotalReceiptsResp::getTotalCardAmount).sum();
