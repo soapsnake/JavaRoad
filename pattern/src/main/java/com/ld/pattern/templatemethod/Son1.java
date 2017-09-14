a6eb1d72769ab9417995c0268d0f28eeb5954024
liudun03 <liudun03@meituan.com>
2017-09-14 15:27:21 +0800

    fix

---
+++ pattern/src/main/java/com/ld/pattern/templatemethod/Son1.java
@@ -0,0 +1,25 @@
+package com.ld.pattern.templatemethod;
+
+/**
+ * Created by liudun on 2017/8/24.
+ */
+public class Son1 extends Absparent {
+    @Override
+    public void overideMethod() {
+
+        super.parentMethod();
+
+        this.parentMethod();
+
+
+        parentMethod();
+
+        System.out.println("son1 oveeride method");
+    }
+
+////    @Override
+//    public void parentMethod(){
+//        System.out.println("son overide parent method");
+//    }
+
+}
--- zookeeper/src/main/java/com/ld/zk/client/ZooKeeperTest.java
+++ zookeeper/src/main/java/com/ld/zk/client/ZooKeeperTest.java
@@ -25,41 +25,41 @@ public class ZooKeeperTest {
             }
         });
 
-        // 创建一个目录节点
+        // 同步模式创建一个目录节点
         zk.create("/testRootPath", "testRootData".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE,
-                CreateMode.EPHEMERAL);
+                CreateMode.PERSISTENT);
 
-        // 创建一个子目录节点
+        // 同步模式创建一个子目录节点
         zk.create("/testRootPath/testChildPathOne", "testChildDataOne".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
 
-        //利用getData不监控/testRootPath
-        System.out.println(new String(zk.getData("/testRootPath",false,null)));
-
-        // 利用getChildren监控/testRootPath,并且取出子目录节点列表
-        System.out.println(zk.getChildren("/testRootPath",true));
+        //利用getData获取单节点数据,第二个参数false表明不监控/testRootPath节点
+        System.out.println(new String(zk.getData("/testRootPath", false, null)));
 
         // 修改子目录节点数据
-        zk.setData("/testRootPath/testChildPathOne","modifyChildDataOne".getBytes(),-1);
+        zk.setData("/testRootPath/testChildPathOne", "modifyChildDataOne".getBytes(), -1);
 
         //利用exists监控/testRootPath
-        System.out.println("目录节点状态：["+zk.exists("/testRootPath",true)+"]");
+        System.out.println("目录节点状态：[" + zk.exists("/testRootPath", true));
 
         // 创建另外一个子目录节点
         zk.create("/testRootPath/testChildPathTwo", "testChildDataTwo".getBytes(),
-                ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
+                ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+        // 利用getChildren监控/testRootPath,并且取出子目录节点列表
+        System.out.println(zk.getChildren("/testRootPath", true));
 
         //利用getData监控/testRootPath/testChildPathTwo,
-        System.out.println(""+new String(zk.getData("/testRootPath/testChildPathTwo",true,null)));
+        System.out.println("" + new String(zk.getData("/testRootPath/testChildPathTwo", true, null)));
 
         // 删除子目录节点,testChildPathTwo被监控,因此必然会触发事件
-        zk.delete("/testRootPath/testChildPathTwo",-1);
+        zk.delete("/testRootPath/testChildPathTwo", -1);
 
         ///testRootPath/testChildPathOne上面没有监控点,因此不会被监控!!!!!!!!这个删除语句不会触发事件
-        zk.delete("/testRootPath/testChildPathOne",-1);
+        zk.delete("/testRootPath/testChildPathOne", -1);
 
         // 删除父目录节点
-        zk.delete("/testRootPath",-1);
+        zk.delete("/testRootPath", -1);
 
         //关闭连接
         zk.close();
