81326223e6594643ed78e9e4fc0a86a5f3d18178
liudun03 <liudun03@meituan.com>
2017-09-19 23:50:12 +0800

    fix

--- hbaseclient/src/main/java/PutClient.java
+++ hbaseclient/src/main/java/PutClient.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.compress.Compression;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -37,21 +39,25 @@ public class PutClient {
         System.out.println("Connected to HBase");
 
         //获取hbase中所有的表
-        System.out.println("this tables exit in hbase: " +Arrays.toString(admin.listTableNames()));
+        System.out.println("this tables in hbase: " +Arrays.toString(admin.listTableNames()));
 
 
         //创建表
         HTableDescriptor newtable = new HTableDescriptor(Bytes.toBytes("testtable"));
         HColumnDescriptor cf = new HColumnDescriptor(Bytes.toBytes("colfam1")).setCompressionType(Compression.Algorithm.NONE);
         newtable.addFamily(cf);
-        System.out.print("Creating table. ");
+
+        HTableDescriptor newtable2 = new HTableDescriptor(Bytes.toBytes("testtable2"));
+        HColumnDescriptor cf2 = new HColumnDescriptor(Bytes.toBytes("colfam2-1")).setCompressionType(Compression.Algorithm.NONE);
+        newtable2.addFamily(cf2);
+
         try {
-            admin.createTable(newtable);
-            System.out.println(" Done.");
-            System.out.println("Tables created");
+          System.out.print("Creating table: ");
+            admin.createTable(newtable2);
+            System.out.println("create table success!");
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("create table failed!!!");
+            System.out.println("create table failed!!");
         }
 
         //删除表
@@ -59,7 +65,6 @@ public class PutClient {
         //修改表
 
         //查看表的信息
-
 
 
 
@@ -76,17 +81,93 @@ public class PutClient {
         puts.add(put);
         table.put(puts);        //实际是一个rpc调用
 
+        Put put1 = new Put(Bytes.toBytes("row1"));
+        put1.add(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"));
+        //CAS操作 compare and set 原子操作
+        try {
+            boolean putRes = table.checkAndPut(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"), null, put1);
+            System.out.println("CAS put res: " + putRes);
+
+            //也可以调用这个方法:
+            Object[] processRes = new Object[puts.size()];   //接受结果的数组
+
+            //void 版batch,这个方法抛出异常后仍然会填充部分结果到processRes当中
+            table.batch(puts, processRes);
+
+            //返回值版batch,这个方法假如抛了异常将会没有任何返回值,已弃用
+            Object[] processRes1 = table.batch(puts);
+
+            System.out.println(Arrays.toString(processRes));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
         //对testtable这张表进行delete操作  删
         Delete delete = new Delete(Bytes.toBytes("row1"));
-        table.delete(delete);
+        //指定delete操作要作用的列族/列定义符,注意,hbase任何的增删改查操作都要带列,否则就是一个空操作
+        //delete操作必须指定要删除的keyvalue的时间戳或者版本号,一个单元格最多保留3个版本,所以,要想把一个单元格删空,需要执行三次指定时间戳的删除操作才行
+        //假如删除的列族+列名对应的keyvalue单元格已经为空,再次删除也不会报错,说明列族+列名仍然存在
+        delete.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), 1505751025065L);
+
+        //但是,如果尝试删除不存在的列族(列名不会),将会抛出异常
+//        delete.addColumn(Bytes.toBytes("colfam3"), Bytes.toBytes("qual4"), 1505751025065L);
+
+        try {
+            table.delete(delete);
+
+            boolean deleteRes = table.checkAndDelete(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"), Bytes.toBytes("val1"),null,delete);
+            System.out.println("CAS delete, res:" + deleteRes);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         table.flushCommits();    //缓冲区激活后需要调这个函数发生实际远程调用
+
 
 
         //对testtable表进行get操作     查
         Get get = new Get(Bytes.toBytes("row1"));
         Result result = table.get(get);
         System.out.println("get row1 result: " + result);
+
+        //精确版get
+        get.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"));
+        Result result1 = table.get(get);
+        byte[] val = result.getValue(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"));
+        System.out.println("value: " + Bytes.toString(val));
+
+
+        //scan操作
+        Person person = new Person();
+        person.setName("liu");
+        person.setAge(18);
+        person.setGender(1);
+        HTable table2 = new HTable(conf, "testtable2");
+        Put put2 = new Put(Bytes.toBytes("row2-1"));
+        put2.add(Bytes.toBytes("colfam2-1"), Bytes.toBytes("qual2-1"), Bytes.toBytes(person.toString()));
+        table2.put(put2);
+
+//        boolean putRes2 = table2.checkAndPut(Bytes.toBytes("colfam2-1"), Bytes.toBytes("qual2-1"), Bytes.toBytes("val2-1"), null, put2);
+//        System.out.println("put2 is success?: " + putRes2);
+
+        Scan scan = new Scan();
+//        scan.addFamily(Bytes.toBytes("colfam1"));
+        ResultScanner scanner =  table2.getScanner(scan);
+        System.out.println("scanner: " + scanner.toString());
+        for (Result scanRes : scanner) {
+            System.out.println("scanner process: " + scanRes);
+        }
+        scanner.close();
+
+
         System.out.println("buffer size: " + table.getWriteBufferSize());   //默认缓冲区大小
 
+
+
+
+
+        table.close();
     }
 }
