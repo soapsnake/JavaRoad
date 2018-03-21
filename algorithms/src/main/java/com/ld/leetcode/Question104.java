385e4bbcf07f68c32767bbbb9d7dc47dda3c808d
liudun <liudun@drilver.com>
2018-03-21 15:43:18 +0800

    fix

---
+++ algorithms/src/main/java/com/ld/leetcode/Question104.java
@@ -0,0 +1,20 @@
+package com.ld.leetcode;
+
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+ }
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        TreeNode node = new TreeNode(3);
+
+
+
+        System.out.println("hello");
+        return 0;
+    }
+}
---
+++ random/src/com/kevin/snake/random/RandomTest.java
@@ -0,0 +1,26 @@
+package com.kevin.snake.random;
+
+import java.util.Random;
+
+public class RandomTest {
+    public static void main(String[] args)
+    {
+        Random random = new Random(System.currentTimeMillis());
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+        System.out.println(Math.abs(random.nextLong() % 900) + 300);
+
+
+    }
+}
