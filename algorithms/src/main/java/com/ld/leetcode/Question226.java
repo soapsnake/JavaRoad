688fbb38e69d4f452ce9f50ce3d3479381c63931
liudun <liudun@drilver.com>
2018-04-05 18:22:14 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/Question226.java
+++ algorithms/src/main/java/com/ld/leetcode/Question226.java
@@ -1,9 +1,5 @@
 package com.ld.leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Invert a binary tree.
  *
@@ -24,7 +20,7 @@ public class Question226 {
         int[] nums =new int[] {4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 4, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 3, 3, 4, 3, 3, 3, 4, 4, 4, 4, 4, 3, 4, 3, 3, 3, 3, 3, 4, 4, 3, 3, 3, 4, 4, 4, 3, 4, 3, 4, 3, 4, 4, 4, 3, 4, 3, 4, 3, 4, 4, 3, 3, 4, 3, 4, 3, 3, 3, 4, 3, 3};
         System.out.println(nums.length);
 
-        List<Integer> ls = new ArrayList<Integer>(Arrays.asList(nums));
+//        List<Integer> ls = new ArrayList<Integer>(Arrays.asList(nums));
 
 
     }
