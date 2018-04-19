3c0e4731c62d8a4ad3677cda957a56c49d152f4f
liudun <liudun@drilver.com>
2018-04-19 10:19:10 +0800

    fix

---
+++ algorithms/src/main/java/com/ld/leetcode/Question160.java
@@ -0,0 +1,14 @@
+package com.ld.leetcode;
+
+/**
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ For example, the following two linked lists:
+ */
+public class Question160 {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+
+        return null;
+    }
+}
