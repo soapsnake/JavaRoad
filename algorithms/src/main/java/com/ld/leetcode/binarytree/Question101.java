7360b2bd93be3817e0b9173ffb2e14daf945a3d5
liudun <liudun@drilver.com>
2018-06-11 10:20:10 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/binarytree/Question101.java
+++ algorithms/src/main/java/com/ld/leetcode/binarytree/Question101.java
@@ -6,50 +6,16 @@ package com.ld.leetcode.binarytree;
  */
 public class Question101 {
     public boolean isSymmetric(TreeNode root) {
-        TreeNode revertRoot = revertTree(root);
-        return traverseTree(root , revertRoot);
-    }
 
-    private boolean traverseTree(TreeNode root, TreeNode revertRoot) {
-        boolean res = true;
-
-        if (root == null){
-            return revertRoot == null;
-        }
-
-        if (root.val != revertRoot.val){
-            res =  false;
-        }
-        boolean resleft = traverseTree(root.left, revertRoot.left);
-        boolean resright = traverseTree(root.right, revertRoot.right);
-        return res;
-    }
-
-    private TreeNode revertTree(TreeNode root) {
-        if (root == null){
-            return null;
-        }
-        TreeNode tmp;
-        tmp =  root.left;
-        root.left = root.right;
-        root.right = tmp;
-
-        revertTree(root.left);
-        revertTree(root.right);
-        return root;
+        //TODO 这题还无解
+        return false;
     }
 
     public static void main(String[] args) {
         TreeNode origin = TreeNode.makeTree();
-        TreeNode.layerTravse(origin);
-
         System.out.println("======================================");
 
         Question101 question101 = new Question101();
-        TreeNode revese = question101.revertTree(origin);
-        TreeNode.layerTravse(revese);
-
         System.out.println(question101.isSymmetric(origin));
-
     }
 }
