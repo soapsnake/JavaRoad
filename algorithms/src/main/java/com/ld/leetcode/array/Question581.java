d21634fad5080f4f78abadd50a0cd899f8d8ec03
liudun <liudun@drilver.com>
2018-07-03 15:43:54 +0800

    fix

---
+++ algorithms/src/main/java/com/ld/leetcode/array/Question581.java
@@ -0,0 +1,15 @@
+package com.ld.leetcode.array;
+
+/**
+ * Given an integer array, you need to find one continuous subarray that if you only sort this subarray in ascending order, then the whole array will be sorted in ascending order, too.
+ You need to find the shortest such subarray and output its length.
+ 老实说这个题目我压根就没有看懂
+ */
+public class Question581 {
+
+    public int findUnsortedSubarray(int[] nums) {
+
+        //TODO 还没有解决
+        return 0;
+    }
+}
---
+++ concurrence/src/main/java/com/ld/aqs/valatile/Worker1.java
@@ -0,0 +1,25 @@
+package com.ld.aqs.valatile;
+
+public class Worker1 implements Runnable {
+    Person person;
+    long start;
+    Worker1(Person person){
+        this.person = person;
+        this.start = System.currentTimeMillis();
+    }
+
+    @Override
+    public void run() {
+        for (;;){
+            try {
+                Thread.sleep(1000L);
+                System.out.println(person.age);
+                if (System.currentTimeMillis() - start > 15 * 1000L){
+                    break;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
