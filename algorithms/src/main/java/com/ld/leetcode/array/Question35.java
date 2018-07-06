ed65750e6271a6979584e23b84992f4412251b34
liudun <liudun@drilver.com>
2018-07-06 14:30:38 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/array/Question35.java
+++ algorithms/src/main/java/com/ld/leetcode/array/Question35.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class Question35 {
 
+    //二分法思想
     public int searchInsert(int[] nums, int target) {
         if (nums == null || nums.length == 0){
             return 0;
@@ -25,6 +26,26 @@ public class Question35 {
         //TODO 解法有问题,没解全
         int[] nums = new int[]{1,2,3,4,5,7,8,10};
         Question35 question35 = new Question35();
-        System.out.println(question35.searchInsert(nums, 8));
+        System.out.println(question35.searchInsert2(nums, 8));
+    }
+
+    public int searchInsert2(int[] nums, int target){
+        if (null == nums){
+            return 0;
+        }
+        if (nums.length == 1){
+            return target > nums[0] ? 1: 0;
+        }
+        if (target < nums[0]){
+            return 0;
+        }
+        for (int i =0 ;i< nums.length;i++){
+            if (nums[i] == target){
+                return i;
+            }else if (target > nums[i] && i+1 <nums.length && target < nums[i+1]){
+                return i+1;
+            }
+        }
+        return nums.length;
     }
 }
