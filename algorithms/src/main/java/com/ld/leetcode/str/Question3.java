f7392ff5207a6e843258f4d3c7db1d3e0691f177
liudun <liudun@drilver.com>
2018-07-12 15:58:44 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/str/Question3.java
+++ algorithms/src/main/java/com/ld/leetcode/str/Question3.java
@@ -2,6 +2,7 @@ package com.ld.leetcode.str;
 
 public class Question3 {
 
+    //最傻逼解法,时间复杂度平方级别
     public int lengthOfLongestSubstring(String s) {
         if (s == null) return 0;
         if (s .equals("")) return 0;
@@ -11,6 +12,7 @@ public class Question3 {
             String temp = "";
             for (int i=0;i<chars.length;i++){
                 temp += chars[i];
+                if (chars.length - i <= max) return max;
                 for (int j=i+1;j<chars.length;j++){
                     if (temp.contains(chars[j] + "")){
                         break;
---
+++ algorithms/src/main/java/com/ld/leetcode/str/Question5.java
@@ -0,0 +1,17 @@
+package com.ld.leetcode.str;
+
+public class Question5 {
+
+    public String longestPalindrome(String s) {
+
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Question5 question5 = new Question5();
+        String str1 = "babad";
+        String str2 = "cbbd";
+        System.out.println(question5.longestPalindrome(str1));
+    }
+}
