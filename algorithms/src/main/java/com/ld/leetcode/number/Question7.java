94f7cbb0992bfa2407f9950ecd9cc8eb9261c73b
liudun <liudun@drilver.com>
2018-07-24 14:15:16 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/number/Question7.java
+++ algorithms/src/main/java/com/ld/leetcode/number/Question7.java
@@ -41,4 +41,21 @@ class Question7 {
         System.out.println(question7.reverse("9646324351"));
 
     }
+
+    //简单版本
+    public int reverse2(int x)
+    {
+        int result = 0;
+        while (x != 0)
+        {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result)
+            { return 0; }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
 }
