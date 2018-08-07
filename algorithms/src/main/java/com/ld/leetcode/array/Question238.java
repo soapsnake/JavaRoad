dba8eb237cf1b894f09ca28b7b8a4a72d2224564
liudun <liudun@drilver.com>
2018-08-07 14:09:01 +0800

    fix

---
+++ algorithms/src/main/java/com/ld/leetcode/array/Question238.java
@@ -0,0 +1,19 @@
+package com.ld.leetcode.array;
+
+public class Question238 {
+
+    /**
+     * Given an array nums of n integers where n > 1,
+     * return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+     *
+     * Example:
+     Input:  [1,2,3,4]
+     Output: [24,12,8,6]
+     */
+    public int[] productExceptSelf(int[] nums) {
+
+        return null;
+    }
+
+
+}
