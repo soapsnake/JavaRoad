09d5c1a7601ab922741168638ea22cee86cc8f58
liudun <liudun@youzan.com>
2018-10-11 10:47:36 +0800

    fix

--- algorithms/src/main/java/com/ld/leetcode/str/Question709.java
+++ algorithms/src/main/java/com/ld/leetcode/str/Question709.java
@@ -6,8 +6,17 @@ package com.ld.leetcode.str;
 public class Question709 {
     public String toLowerCase(String str) {
 
-
-        return null;
+        char[] strings = str.toCharArray();
+        String newstr = "";
+        for (char s: strings){
+            if (s >= 'A' && s <= 'Z'){
+                //s大写了，需要转小写
+                newstr += (char)(s - ('A' - 'a'));
+            }else {
+                newstr += s;
+            }
+        }
+        return newstr;
     }
 
 }
