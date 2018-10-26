7149fb6c842d9b0df4a6c08581aac9d53dc070ee
liudun <liudun@youzan.com>
2018-10-26 10:49:54 +0800

    fix

---
+++ algorithms/src/main/java/com/ld/leetcode/number/Question476.java
@@ -0,0 +1,19 @@
+package com.ld.leetcode.number;
+
+/**
+ * Given a positive integer, output its complement number.
+ * The complement strategy is to flip the bits of its binary representation.
+ */
+public class Question476 {
+
+    // 老实说这个解法根本就看不懂
+    public int findComplement(int num) {
+        return ~num + (Integer.highestOneBit(num) << 1);
+    }
+
+    public static void main(String[] args) {
+        Question476 question476 = new Question476();
+        System.out.println(question476.findComplement(5));
+    }
+
+}
