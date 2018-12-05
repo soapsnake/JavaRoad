f911e599ea97f247694b0e0299d2047452c0c294
liudun <liudun@youzan.com>
2018-12-05 10:27:38 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question387.java
@@ -0,0 +1,24 @@
+package com.soapsnake.algorithms.leetcode.str;
+
+/**
+ * @Auther soapsnake@gmail.com
+ * @Date 2018/12/4 19:15
+ */
+public class Question387 {
+
+    public int firstUniqChar(String s) {
+
+        //todo freqen[]
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Question387 question387 = new Question387();
+        String tar = "loveleetcode";
+
+        String tar1 = "cc";
+
+        System.out.println(question387.firstUniqChar(tar1));
+    }
+}
