39d354fcde39cd5f5fbf07fe78f1d07c42b48e7e
liudun <liudun@youzan.com>
2018-12-19 00:39:51 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question496.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question496.java
@@ -1,48 +1,39 @@
 package com.soapsnake.algorithms.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author soapsnake
  * @date 2018/11/8
  */
 class Question496 {
 
-    //todo 这道题目并不简单
+    //真他妈简单,反过来想,nums2中的数字,先按预期放进map中,然后用nums1中对应元素去取就行了
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        //nums1 是 nums2的子数组,两个数组都是无序的
         int[] res = new int[nums1.length];
-        int des = 0;
-        int index = 0;
+        Map<Integer, Integer> hashMap = new HashMap<>();
         for (int i = 0; i < nums2.length; i++) {
-            if (index == nums1.length) {
-                break;
-            }
-            for (int j = 0; j < nums1.length; j++) {
-                if (nums2[j] == nums1[i]) {
-                    des = nums1[i];
-                    break;
+            for (int j = i + 1; j < nums2.length; j++) {
+                if (nums2[j] > nums2[i]) {
+                    hashMap.put(nums2[i], nums2[j]);//i元素右侧出现了比它大的
+                    break; //只需要第一个比它大的,break防止出现更大的进行了覆盖
                 }
             }
-            if (des == 0) {
-                continue;
-            }
-
-            for (int k = i + 1; k < nums2.length; k++) {
-                if (nums2[k] > des) {
-                    res[index++] = nums2[k];
-                }
-            }
-            res[index++] = -1;
+            hashMap.putIfAbsent(nums2[i], -1); //i元素右侧没有比它大的,那就放个-1
         }
 
-
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = hashMap.get(nums1[i]); //直接取,因为nums1是nums2的子数组,所以不用防空
+        }
         return res;
     }
 
     public static void main(String[] args) {
         Question496 question496 = new Question496();
 
-        int[] nums1 = {4,1,2};
-        int[] nums2 = {1,2,4,2};
+        int[] nums1 = {2, 4};
+        int[] nums2 = {1,2, 3,4};
 
         ArrayUtils.printArr(question496.nextGreaterElement(nums1, nums2));
     }
