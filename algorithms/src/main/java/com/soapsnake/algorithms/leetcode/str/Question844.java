bd8feee33c3c6cf9679288ac9bdddf5061bd41e7
liudun <liudun@youzan.com>
2018-12-23 01:22:23 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question844.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question844.java
@@ -5,11 +5,47 @@ package com.soapsnake.algorithms.leetcode.str;
  * @Date 2018-12-20 00:56
  */
 public class Question844 {
-
     public boolean backspaceCompare(String S, String T) {
         char[] charS = S.toCharArray();
         char[] charT = T.toCharArray();
+        this.iteer(charS);
+        this.iteer(charT);
+        String resS = this.buildStr(charS);
+        String resT = this.buildStr(charT);
+        return resS.equals(resT);
+    }
 
-        return false;
+    private String buildStr(char[] chars) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : chars) {
+            if (c != ' ') {
+                stringBuilder.append(c);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private void iteer(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '#') {
+                chars[i] = ' ';
+                for (int j = i;j >= 0;) {
+                    if (chars[j] != ' ') {
+                        chars[j] = ' ';
+                        break;
+                    }else {
+                        j--;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Question844 question844 = new Question844();
+        String s = "xywrrmp";
+        String t = "xywrrmu#p";
+        System.out.println(question844.backspaceCompare(s, t));
     }
 }
