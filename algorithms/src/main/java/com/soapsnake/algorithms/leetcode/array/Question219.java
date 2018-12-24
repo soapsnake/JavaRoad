daaf23ed2149d9f2b9531cd4c8e054a7cd778c0e
liudun <liudun@youzan.com>
2018-12-24 23:53:45 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question219.java
@@ -0,0 +1,39 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @Auther soapsnake@gmail.com
+ * @Date 2018-12-24 00:16
+ */
+public class Question219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    if (Math.abs(i - j) <= k) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Question219 question219 = new Question219();
+        int[] nums = {1,2,3,1,2,3};
+        int k = 2;
+        System.out.println(question219.containsNearbyDuplicate2(nums, k));
+    }
+
+    //todo 思想:把数字缓存起来,k步内验证是否有重,k步后把该数字清除
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+
+
+        return false;
+    }
+}
--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question917.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question917.java
@@ -1,7 +1,7 @@
 package com.soapsnake.algorithms.leetcode.str;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author soapsnake
@@ -9,33 +9,79 @@ import java.util.List;
  */
 class Question917 {
 
+    /**
+     * Input: "a-bC-dEf-ghIj"
+     * Output: "j-Ih-gfE-dCba"
+     */
     public String reverseOnlyLetters(String S) {
-        /**
-         * todo 解法是错的,题目要求除了23个英语字母外其他符号都不能动,这里只处理了'-'
-         * 那么这个解法其实也只能处理一种变异符号,一种以上就没办法了,还是得用双指针
-         */
-
         char[] chars = S.toCharArray();
         StringBuilder stringBuilder = new StringBuilder();
-        List<Integer> index = new ArrayList<>();
+
+        /**
+         * 这里为什么要使用treeMap:如果用hashmap,等会儿插入这个符号的时候没办法按照
+         * 索引从小到大顺序插入(hashmap的key无顺序),就会造成arrayindexoutofbound
+         */
+        Map<Integer, Character> index = new TreeMap<>();
         int i = 0;
         for (char c : chars) {
-            if (c == '-') {
-                index.add(i);
+            if (!this.isCharactor(c)) { //c是特殊字符
+                index.put(i, c);
             } else {
                 stringBuilder.insert(0, c + "");
             }
             i++;
         }
-            for (Integer integer : index) {
-                stringBuilder.insert(integer, "-");
-            }
+
+        for (Map.Entry<Integer, Character> entry : index.entrySet()) {
+            stringBuilder.insert(entry.getKey(), entry.getValue() + "");
+        }
         return stringBuilder.toString();
     }
 
+    private boolean isCharactor(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return true;
+        }
+
+        return c >= 'A' && c <= 'Z';
+    }
+
     public static void main(String[] args) {
-        String s = "a-bC-dEf-ghIj"; // j-Ih-gfE-Cb-a
+        String s = "a-bC-dEf-ghIj";
+                 //"j-Ih-gfE-dCba"
         Question917 question917 = new Question917();
-        System.out.println(question917.reverseOnlyLetters(s));
+        System.out.println(question917.reverseOnlyLetters2(s));
+    }
+
+    //类似快排的左右指针碰撞,只要左右字符不是特殊字符就交换
+    public String reverseOnlyLetters2(String S) {
+        char[] chars = S.toCharArray();
+        int left = 0;
+        int right = S.length() - 1;
+        while (right > left) {
+            while (left < right) {
+                if (Character.isLetter(chars[left])) {
+                    break;
+                }
+                left++;
+            }
+
+            while (right > left) {
+                if (Character.isLetter(chars[right])) {
+                    break;
+                }
+                right--;
+            }
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;   //容易遗漏这里
+            right--;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : chars) {
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
     }
 }
