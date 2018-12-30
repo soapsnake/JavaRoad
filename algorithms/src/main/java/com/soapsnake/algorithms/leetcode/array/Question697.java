cbc4dc933ad0bab180689810be2b2b0fa2a647de
liudun <liudun@youzan.com>
2018-12-30 22:00:26 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question697.java
@@ -0,0 +1,26 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @Auther soapsnake@gmail.com
+ * @Date 2018-12-30 21:23
+ */
+public class Question697 {
+
+    /**
+     * 1. nums中有重复数字,该重复的数字重复次数记为n
+     * 2. 则nums的度记为Max(n)
+     * 3. 找出nums的连续子数组中,能够达到Max(n)的最短子数组
+     * 4. 输出该最短子数组的长度res
+     *
+     */
+    public int findShortestSubArray(int[] nums) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Question697 question697 = new Question697();
+        int[] nums = {1,3,2,2,3,1};
+        System.out.println(question697.findShortestSubArray(nums));
+    }
+
+}
