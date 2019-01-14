5fa52366ea7a476cf45deef158b41f11692fb64e
liudun <liudun@youzan.com>
2019-01-14 23:29:42 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question350.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question350.java
@@ -2,7 +2,9 @@ package com.soapsnake.algorithms.leetcode.array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Auther soapsnake@gmail.com
@@ -11,25 +13,28 @@ import java.util.List;
 public class Question350 {
 
     public int[] intersect(int[] nums1, int[] nums2) {
-        List<Integer> temp = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
         List<Integer> res = new ArrayList<>();
-
         for (int i = 0; i < nums1.length; i++) {
-            temp.add(nums1[i]);
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
+            }
         }
 
         for (int i = 0; i < nums2.length; i++) {
-            if (temp.contains(nums2[i])) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
                 res.add(nums2[i]);
+                map.put(nums2[i] ,map.get(nums2[i]) - 1);
             }
         }
-        int[] resarr = new int[res.size()];
-        int j = 0;
-        for (Integer i : res) {
-            resarr[j++] = i;
-        }
 
-        return  resarr;
+        int[] resArr = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            resArr[i] = res.get(i);
+        }
+        return resArr;
     }
 
     public static void main(String[] args) {
