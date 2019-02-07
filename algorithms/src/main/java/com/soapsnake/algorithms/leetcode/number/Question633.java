cb9e1b534978a206c0d528faf0db2816659572ed
liudun <liudun@youzan.com>
2019-02-07 23:59:32 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/number/Question633.java
@@ -0,0 +1,27 @@
+package com.soapsnake.algorithms.leetcode.number;
+
+/**
+ * @Auther soapsnake@gmail.com
+ * @Date 2019-02-07 23:53
+ */
+public class Question633 {
+    public boolean judgeSquareSum(int c) {
+        if (c == 0) {
+            return true;
+        }
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+        //左右指针
+        while (right >= left) {
+            int dest = left * left + right * right;
+            if (dest > c) {
+                right--;
+            } else if (dest < c) {
+                left++;
+            } else {
+                return true; //找到符合的结果
+            }
+        }
+        return false;
+    }
+}
