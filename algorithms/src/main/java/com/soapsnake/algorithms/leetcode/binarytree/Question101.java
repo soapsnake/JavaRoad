4b334fdd78c9696b7e8f7d76d6988f381681a689
liudun <liudun@youzan.com>
2019-02-19 11:13:07 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/binarytree/Question101.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/binarytree/Question101.java
@@ -1,5 +1,9 @@
 package com.soapsnake.algorithms.leetcode.binarytree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
  * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
@@ -11,12 +15,59 @@ class Question101 {
 
         Question101 question101 = new Question101();
         System.out.println(question101.isSymmetric(origin));
-
     }
 
-    public boolean isSymmetric(TreeNode root) {
-
-        //TODO 这题还无解
+    public boolean isSymmetric2(TreeNode root) {
+        //todo 思路2,利用递归,入参为两个节点;
         return false;
     }
+
+        //思路:层状遍历,每一层都是对称结构,可用左右指针碰撞算法校验每一层是否对称
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return false;
+        }
+        return this.layer(root);
+    }
+
+    private boolean layer(TreeNode root) {
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<TreeNode> row = null;
+        while (!queue.isEmpty()) {
+            row = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                if (node != null)
+                row.add(node);
+            }
+            int left = 0;
+            int right = row.size() - 1;
+            while (right > left) {
+                if (row.get(left) == null && row.get(right) != null) {
+                    return false;
+                }
+                if (row.get(left) != null && row.get(right) == null) {
+                    return false;
+                }
+                if (row.get(left) == null && row.get(right) == null) {
+                    right--;
+                    left++;
+                    continue;
+                }
+                if (row.get(left).val != row.get(right).val) {
+                    return false;
+                } else {
+                    right--;
+                    left++;
+                }
+            }
+            for (TreeNode node : row) {
+                if (node == null) continue;
+                queue.add(node.left);
+                queue.add(node.right);
+            }
+        }
+        return true;
+     }
 }
