a466837ba428ffe8ca4741a2dff4b46894dfd064
liudun <liudun@youzan.com>
2019-03-14 14:59:21 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question39.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question39.java
@@ -1,6 +1,7 @@
 package com.soapsnake.algorithms.leetcode.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -19,26 +20,35 @@ public class Question39 {
      * ]
      */
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            List<List<Integer>> res = new ArrayList<>();
-            //1. 能整除
-            int left = 0;
-            int right = candidates.length - 1;
-            while (right > left) {
-                while (left < right) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        dfsTree(res, new ArrayList<>(), candidates, target, 0);
+        return  res;
+    }
 
-                }
-
-
-
+    private void dfsTree(List<List<Integer>> res, ArrayList<Integer> tmp, int[] candidates, int remain, int start) {
+        if (remain < 0) {
+            return;
+        } else if (remain == 0) {
+            /**
+             *  最关键的就是这个new ArrayList,由于tmp后续会改变,所以如果不copy出来一个新对象,
+             *  那么tmp中的值会一直变,res也会跟着变,导致最后结果不准确
+             */
+            res.add(new ArrayList<>(tmp));
+            return;
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                tmp.add(candidates[i]);
+                dfsTree(res, tmp, candidates, remain - candidates[i], i); //这里为什么用i
+                tmp.remove(tmp.size() - 1);  //这个remove操作完成了回退操作,非常的神奇
             }
+        }
+    }
 
-
-
-            //2. 求和
-
-           //3. 一个数的乘方与其他数求和
-
-
-            return  res;
+    public static void main(String[] args) {
+        Question39 question39  = new Question39();
+        int[] nums = {2,3,6,7};
+        int tartget = 7;
+        System.out.println(question39.combinationSum(nums, tartget));
     }
 }
