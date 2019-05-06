6474e3322d8d1015ec01836306c05f40374c599e
liudun <liudun@youzan.com>
2019-05-06 11:08:51 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question152.java
@@ -0,0 +1,18 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+public class Question152 {
+
+    /**
+     * Input: [2,3,-2,4]
+     * Output: 6
+     * Explanation: [2,3] has the largest product 6.
+     */
+    public int maxProduct(int[] nums) {
+
+
+        int max = 0;
+
+
+        return max;
+    }
+}
