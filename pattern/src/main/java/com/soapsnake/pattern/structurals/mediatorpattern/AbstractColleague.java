640dbde4ebc8436836495e4490a53c0c52afcdde
soapsnake <soapsnake@gmail.com>
2019-09-28 14:26:41 +0800

    this is a rebase to modify 'fix'

---
+++ pattern/src/main/java/com/soapsnake/pattern/structurals/mediatorpattern/AbstractColleague.java
@@ -0,0 +1,10 @@
+package com.soapsnake.pattern.structurals.mediatorpattern;
+
+public class AbstractColleague implements Colleagure {
+
+	protected String type;
+
+	public AbstractColleague(String type) {
+		this.type = type;
+	}
+}
