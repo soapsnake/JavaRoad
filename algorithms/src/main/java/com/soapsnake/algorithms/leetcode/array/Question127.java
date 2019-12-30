14114026fefebce3990a6d77599814998034dc29
soapsnake <soapsnake@gmail.com>
2019-12-30 23:34:39 +0800

    739

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question127.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question127.java
@@ -1,6 +1,8 @@
 package com.soapsnake.algorithms.leetcode.array;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author
@@ -28,17 +30,49 @@ import java.util.List;
  */
 public class Question127 {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        if (wordList.isEmpty()) {
-            return 0;
-        }
         if (!wordList.contains(endWord)) {
             return 0;
         }
+        Set<String> beginSet = new HashSet<>(), endSet = new HashSet<>();
+        int len = 1;
+        Set<String> visited = new HashSet<>();
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            //如果begin大于end,那么交换两个set
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
 
+            Set<String> temp = new HashSet<>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+                for (int i = 0; i < chs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chs[i];
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
 
+                        if (endSet.contains(target)) {
+                            //已经到达结果,可以返回了
+                            return len + 1;
+                        }
 
-
-
+                        //还没到结果
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            //temp装的是wordlist中包含的合法字串,而且之前没有被访问过
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                        chs[i] = old;
+                    }
+                }
+            }
+            beginSet = temp;
+            len++;
+        }
         return 0;
     }
 }
