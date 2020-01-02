f9b791cbf6feaa83e2e0e7009323c5a97a6c8e5b
liudun <liudun@kuaishou.com>
2020-01-02 09:48:49 +0800

    134

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question134.java
@@ -0,0 +1,32 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-01-02
+ */
+public class Question134 {
+
+    /**
+     * Example 1:
+     *
+     * Input:
+     * gas  = [1,2,3,4,5]
+     * cost = [3,4,5,1,2]
+     *
+     * Output: 3
+     *
+     * Explanation:
+     * Start at station 3 (index 3) and fill up with 4 unit of gas. Your tank = 0 + 4 = 4
+     * Travel to station 4. Your tank = 4 - 1 + 5 = 8
+     * Travel to station 0. Your tank = 8 - 2 + 1 = 7
+     * Travel to station 1. Your tank = 7 - 3 + 2 = 6
+     * Travel to station 2. Your tank = 6 - 4 + 3 = 5
+     * Travel to station 3. The cost is 5. Your gas is just enough to travel back to station 3.
+     * Therefore, return 3 as the starting index.
+     */
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+
+        return 0;
+    }
+}
