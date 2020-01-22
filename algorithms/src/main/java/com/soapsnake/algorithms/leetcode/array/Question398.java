c72e9952f31ed55dba0f8547793095e04d97275f
liudun <liudun@kuaishou.com>
2020-01-22 20:00:15 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question398.java
@@ -0,0 +1,56 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+import java.awt.font.NumericShaper;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import sun.text.resources.cldr.ur.FormatData_ur_IN;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-01-22
+ */
+public class Question398 {
+
+    //pick要求从nums中挑出target的索引,nums中数字可能存在重复
+    static class Solution {
+        private int[] nums;
+        private Random random = new Random();
+        public Solution(int[] nums) {
+            this.nums = nums;
+        }
+
+        /**
+         * To those who don't understand why it works. Consider the example in the OJ
+         * {1,2,3,3,3} with target 3, you want to select 2,3,4 with a probability of 1/3 each.
+         *
+         * 2 : It's probability of selection is 1 * (1/2) * (2/3) = 1/3
+         * 3 : It's probability of selection is (1/2) * (2/3) = 1/3
+         * 4 : It's probability of selection is just 1/3
+         * So they are each randomly selected.
+         */
+        public int pick(int target) {
+            int result = -1;
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != target)
+                    continue;
+                if (random.nextInt(++count) == 0)
+                    result = i;
+            }
+
+            return result;
+        }
+
+        public int pick2(int target) {
+            List<Integer> list = new ArrayList<>();  //index
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    list.add(i);
+                }
+            }
+            return list.get(random.nextInt(list.size()));
+        }
+    }
+}
--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question388.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question388.java
@@ -1,5 +1,7 @@
 package com.soapsnake.algorithms.leetcode.str;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -9,7 +11,19 @@ import java.util.List;
 public class Question388 {
 
     public int lengthLongestPath(String input) {
-
-        return 0;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0); // "dummy" length
+        int maxLen = 0;
+        for (String s : input.split("\n")) {
+            int lev = s.lastIndexOf("\t") + 1; // number of "\t"
+            while (lev + 1 < stack.size())
+                stack.pop(); // find parent
+            int len = stack.peek() + s.length() - lev + 1; // remove "/t", add"/"
+            stack.push(len);
+            // check if it is file
+            if (s.contains("."))
+                maxLen = Math.max(maxLen, len - 1);
+        }
+        return maxLen;
     }
 }
