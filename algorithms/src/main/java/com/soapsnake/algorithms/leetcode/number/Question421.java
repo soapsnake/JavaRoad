fccd9ebcc385c07b5975540da623445c4aa9490c
liudun <liudun@kuaishou.com>
2020-02-24 23:31:02 +0800

    will 421

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/number/Question421.java
@@ -0,0 +1,15 @@
+package com.soapsnake.algorithms.leetcode.number;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-02-24
+ */
+public class Question421 {
+
+    //找到数组中任意两个数字亦或结果的最大值
+    public int findMaximumXOR(int[] nums) {
+
+
+        return 0;
+    }
+}
