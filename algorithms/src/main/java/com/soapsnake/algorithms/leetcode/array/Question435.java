0440911ca77db09088188d4c2ab800e16039c3ab
liudun <liudun@kuaishou.com>
2020-02-29 23:20:21 +0800

    will 435

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question435.java
@@ -0,0 +1,30 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-02-29
+ */
+public class Question435 {
+
+    /**
+     * Example 1:
+     * Input: [[1,2],[2,3],[3,4],[1,3]]
+     * Output: 1
+     * Explanation: [1,3] can be removed and the rest of intervals are non-overlapping.
+
+     * Example 2:
+     * Input: [[1,2],[1,2],[1,2]]
+     * Output: 2
+     * Explanation: You need to remove two [1,2] to make the rest of intervals non-overlapping.
+
+     * Example 3:
+     * Input: [[1,2],[2,3]]
+     * Output: 0
+     * Explanation: You don't need to remove any of the intervals since they're already non-overlapping.
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+
+        return 0;
+    }
+}
