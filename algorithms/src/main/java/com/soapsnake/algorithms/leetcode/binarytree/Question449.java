bf1a459c16651108dcc3cc2963cadc3aae4d5d9b
liudun <liudun@kuaishou.com>
2020-03-05 23:40:43 +0800

    will-449

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/binarytree/Question449.java
@@ -0,0 +1,26 @@
+package com.soapsnake.algorithms.leetcode.binarytree;
+
+import com.soapsnake.algorithms.structures.tree.TreeNode;
+
+import jdk.nashorn.internal.ir.ReturnNode;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-05
+ */
+public class Question449 {
+
+    public class Codec {
+
+        // Encodes a tree to a single string.
+        public String serialize(TreeNode root) {
+
+            return null;
+        }
+
+        // Decodes your encoded data to tree.
+        public TreeNode deserialize(String data) {
+            return null;
+        }
+    }
+}
