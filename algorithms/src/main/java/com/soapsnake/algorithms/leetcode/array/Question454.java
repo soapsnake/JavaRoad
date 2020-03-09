f029635a7b6e9ce8bad7bf619e927a27a516bfff
liudun <liudun@kuaishou.com>
2020-03-09 23:54:20 +0800

    will_457

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question454.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question454.java
@@ -1,5 +1,10 @@
 package com.soapsnake.algorithms.leetcode.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.tools.javac.code.Type.ForAll;
+
 /**
  * @author liudun <liudun@kuaishou.com>
  * Created on 2020-03-08
@@ -23,10 +28,21 @@ public class Question454 {
      * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
      * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
      */
+    //leetcode454
     public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : C) {
+            for (int j : D) {
+                map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+            }
+        }
 
-
-        return 0;
+        int res = 0;
+        for (int l : A) {
+            for (int m : B) {
+                res += map.getOrDefault(-(l + m), 0);
+            }
+        }
+        return res;
     }
-
 }
---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question457.java
@@ -0,0 +1,14 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-09
+ */
+public class Question457 {
+
+    public boolean circularArrayLoop(int[] nums) {
+
+
+        return false;
+    }
+}
