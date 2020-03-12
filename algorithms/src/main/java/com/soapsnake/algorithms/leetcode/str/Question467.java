6117d868f7c74c731a44bff9d1e482fa7e2b0670
liudun <liudun@kuaishou.com>
2020-03-12 23:53:20 +0800

    will_467

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question467.java
@@ -0,0 +1,28 @@
+package com.soapsnake.algorithms.leetcode.str;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-12
+ */
+public class Question467 {
+
+    /**
+     * Example 2:
+     * Input: "cac"
+     * Output: 2
+     * Explanation: There are two substrings "a", "c" of string "cac" in the string s.
+     *
+     *
+     * Example 3:
+     * Input: "zab"
+     * Output: 6
+     * Explanation: There are six substrings "z", "a", "b", "za", "ab", "zab" of string "zab" in the string s.
+     */
+    public int findSubstringInWraproundString(String p) {
+
+
+        return 0;
+    }
+
+
+}
