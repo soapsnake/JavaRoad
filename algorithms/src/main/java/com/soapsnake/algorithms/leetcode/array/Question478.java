26d959e56eae2498ff7a66844df053814cc59729
liudun <liudun@kuaishou.com>
2020-03-17 23:21:36 +0800

    will_478

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question478.java
@@ -0,0 +1,18 @@
+package com.soapsnake.algorithms.leetcode.array;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-17
+ */
+public class Question478 {
+
+    class Solution {
+        public Solution(double radius, double x_center, double y_center) {
+
+        }
+
+        public double[] randPoint() {
+            return null;
+        }
+    }
+}
--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/biner/Question477.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/biner/Question477.java
@@ -6,9 +6,15 @@ package com.soapsnake.algorithms.leetcode.biner;
  */
 public class Question477 {
 
+    //leetcode477
     public int totalHammingDistance(int[] nums) {
-
-
-        return 0;
+        int total = 0, n = nums.length;
+        for (int j = 0; j < 32; j++) {
+            int bitCount = 0;
+            for (int num : nums)
+                bitCount += (num >> j) & 1;
+            total += bitCount * (n - bitCount);
+        }
+        return total;
     }
 }
