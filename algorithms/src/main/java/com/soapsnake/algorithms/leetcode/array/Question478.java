4ac113de84c82c0f03b55b0cbfeab311ebf8dea8
liudun <liudun@kuaishou.com>
2020-03-19 00:12:08 +0800

    will_481

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question478.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question478.java
@@ -6,13 +6,22 @@ package com.soapsnake.algorithms.leetcode.array;
  */
 public class Question478 {
 
+    //leetcode478
     class Solution {
-        public Solution(double radius, double x_center, double y_center) {
+        private double radius, x_center, y_center;
 
+        public Solution(double radius, double x_center, double y_center) {
+            this.radius = radius;
+            this.x_center = x_center;
+            this.y_center = y_center;
         }
 
         public double[] randPoint() {
-            return null;
+            double len = Math.sqrt(Math.random()) * radius;
+            double deg = Math.random() * 2 * Math.PI;
+            double x = x_center + len * Math.cos(deg);
+            double y = y_center + len * Math.sin(deg);
+            return new double[] {x, y};
         }
     }
 }
---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question481.java
@@ -0,0 +1,14 @@
+package com.soapsnake.algorithms.leetcode.str;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-19
+ */
+public class Question481 {
+
+    public int magicalString(int n) {
+
+
+        return 0;
+    }
+}
