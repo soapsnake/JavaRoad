3e1f285aa1b9312ae357ae9a81519752caa52059
liudun <liudun@kuaishou.com>
2020-03-20 11:13:35 +0800

    will_787

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/graph/Question787.java
@@ -0,0 +1,23 @@
+package com.soapsnake.algorithms.leetcode.graph;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-20
+ */
+public class Question787 {
+
+    /**
+     * Example 2:
+     * Input:
+     * n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+     * src = 0, dst = 2, k = 0
+     * Output: 500
+     * Explanation:
+     * The graph looks like this:
+     * The cheapest price from city 0 to city 2 with at most 0 stop costs 500, as marked blue in the picture.
+     */
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+
+        return 0;
+    }
+}
