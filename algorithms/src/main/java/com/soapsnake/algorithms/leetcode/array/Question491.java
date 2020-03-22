b04c2e4bac7da912cbba2055611de71165116d04
liudun <liudun@kuaishou.com>
2020-03-22 23:49:49 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question491.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question491.java
@@ -1,6 +1,9 @@
 package com.soapsnake.algorithms.leetcode.array;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author liudun <liudun@kuaishou.com>
@@ -8,9 +11,27 @@ import java.util.List;
  */
 public class Question491 {
 
+    //leetcode491
     public List<List<Integer>> findSubsequences(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        this.backTrace(new LinkedList<>(), 0, nums, res);
+        return res;
+    }
 
-
-        return null;
+    private void backTrace(LinkedList<Integer> list, int index, int[] nums, List<List<Integer>> res) {
+        if (list.size() > 1) {
+            res.add(new LinkedList<>(list));
+        }
+        Set<Integer> used = new HashSet<>();
+        for (int i = index; i < nums.length; i++) {
+            if (used.contains(nums[i]))
+                continue;
+            if (list.size() == 0 || nums[i] >= list.peekLast()) {
+                used.add(nums[i]);
+                list.add(nums[i]);
+                backTrace(list, i + 1, nums, res);
+                list.remove(list.size() - 1);
+            }
+        }
     }
 }
