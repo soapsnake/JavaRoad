99ac41a90386d732237c2d2784116a9cef6ef75e
liudun <liudun@kuaishou.com>
2020-03-31 23:40:08 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/dp/Question516.java
@@ -0,0 +1,28 @@
+package com.soapsnake.algorithms.leetcode.dp;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-03-31
+ */
+public class Question516 {
+
+    //question516 最长回文字串
+    public int longestPalindromeSubseq(String s) {
+        int[][] dp = new int[s.length()][s.length()];
+        for (int i = s.length() - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
