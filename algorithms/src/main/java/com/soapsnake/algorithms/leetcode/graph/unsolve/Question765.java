2c2f778e8d1049bf813d10f80551115079f75c07
soapsnake <soapsnake@gmail.com>
2020-07-11 00:10:45 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/graph/unsolve/Question765.java
@@ -0,0 +1,23 @@
+package com.soapsnake.algorithms.leetcode.graph.unsolve;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-07-11
+ */
+public class Question765 {
+
+    /**
+     * N对情侣坐在排列成一排的2N个座位上，想牵手。我们想知道最少的交换次数，
+     * 使每对情侣都是并排而坐。一个交换包括选择任意两个人，然后他们站起来并交换座位。
+     * 人和座位用一个从0到2N-1的整数来表示，情侣的编号依次为，
+     * 第一对情侣是（0，1），第二对情侣是（2，3），以此类推，最后一对情侣是（2N-2，2N-1）。
+     *
+     * 情侣的初始座位由row[i]是最初坐在第i-th座位上的人的值给出。
+     * @param row
+     * @return
+     */
+    public int minSwapsCouples(int[] row) {
+        //感觉这道题非常的难
+        return 0;
+    }
+}
