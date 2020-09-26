2c6e381c8bd2a6d0630c08232eb600d689ed2f39
soapsnake <soapsnake@gmail.com>
2020-09-26 23:34:48 +0800

    fix

--- hadoop/src/main/java/com/soapsnake/hadoop/yarn/YarnApplication.java
+++ hadoop/src/main/java/com/soapsnake/hadoop/yarn/YarnApplication.java
@@ -1,16 +1,15 @@
-package com.soapsnake.hadoop.client;
+package com.soapsnake.hadoop.yarn;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Vector;
+import java.util.Set;
 
-import org.apache.commons.collections.BeanMap;
-import org.apache.commons.collections.map.HashedMap;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
@@ -21,62 +20,68 @@ import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.security.token.Token;
 import org.apache.hadoop.yarn.api.ApplicationConstants;
 import org.apache.hadoop.yarn.api.ApplicationConstants.Environment;
-import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationRequest;
 import org.apache.hadoop.yarn.api.protocolrecords.GetNewApplicationResponse;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
+import org.apache.hadoop.yarn.api.records.ApplicationReport;
 import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
 import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
 import org.apache.hadoop.yarn.api.records.LocalResource;
+import org.apache.hadoop.yarn.api.records.Priority;
 import org.apache.hadoop.yarn.api.records.Resource;
 import org.apache.hadoop.yarn.client.api.YarnClient;
 import org.apache.hadoop.yarn.client.api.YarnClientApplication;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
-import org.apache.hadoop.yarn.webapp.hamlet.Hamlet.MAP;
-import org.apache.htrace.shaded.commons.logging.impl.WeakHashtable;
 
-import sun.rmi.runtime.Log;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author liudun <liudun@kuaishou.com>
- * Created on 2020-09-23
+ * Created on 2020-09-26
  */
-public class YarnClientTester {
-
-
-    private static final String SCRIPT_PATH = "";
-    private static Object DSConstants;
+@Slf4j
+public class YarnApplication {
 
     public static void main(String[] args) throws IOException, YarnException {
-        YarnClient yarnClient = YarnClient.createYarnClient();
+        Configuration conf = new Configuration();
 
-        Configuration configuration = new Configuration();
-        yarnClient.init(configuration);
+        //yarnClient的主要作用是方便client提交任务给yarn执行,相当于mvc的controller
+        YarnClient yarnClient = YarnClient.createYarnClient();
+        yarnClient.init(conf);
         yarnClient.start();
 
+        //use yarnclient to create an application
         YarnClientApplication app = yarnClient.createApplication();
+        GetNewApplicationResponse appResponse = app.getNewApplicationResponse();
 
-        GetNewApplicationResponse response = app.getNewApplicationResponse();
+        //设置应用提交上下文
+        ApplicationSubmissionContext appContext = app.getApplicationSubmissionContext();
+        ApplicationId appId = appContext.getApplicationId();
 
-        ApplicationSubmissionContext applicationSubmissionContext = app.getApplicationSubmissionContext();
-        ApplicationId appId = applicationSubmissionContext.getApplicationId();
-        applicationSubmissionContext.setKeepContainersAcrossApplicationAttempts(true);
-        String appName = "firstYarnApp";
-        applicationSubmissionContext.setApplicationName(appName);
+        boolean keepContainers = false;  //是否保留容器??
+        appContext.setKeepContainersAcrossApplicationAttempts(keepContainers);
+        String appName = "soapsnake_first_yarn_task";
+        appContext.setApplicationName(appName);
 
-        Map<String, LocalResource> localResourceMap = new HashMap<>();
+        // set local resources for the application master,local files or archives as needed
+        Map<String, LocalResource> localResources = new HashMap<>();
+        log.info("Copy App Master jar from local filesystem and add to local environment");
 
-        System.out.println("Copy App Master jar from local filesystem and add to local environment");
+        // Copy the application master jar to the filesystem
+        // Create a local resource to point to the destination jar path
+        FileSystem fs = FileSystem.get(conf);
+        String appMasterJar = "";
+        String appMasterJarPath = "";
+        addToLocalResources(fs, appMasterJar, appMasterJarPath, appId.toString(),
+                localResources, null);
 
-        FileSystem fs = FileSystem.get(configuration);
-//        addToLocalResources(fs, appMasterJar, appMasterJarPath, appId.toString(),
-//                localResources, null);
-
+        String log4jPropFile = "";
+        String log4jPath = "";
         // Set the log4j properties if needed
-//        if (!log4jPropFile.isEmpty()) {
-//            addToLocalResources(fs, log4jPropFile, log4jPath, appId.toString(),
-//                    localResources, null);
-//        }
+        if (!log4jPropFile.isEmpty()) {
+            addToLocalResources(fs, log4jPropFile, log4jPath, appId.toString(),
+                    localResources, null);
+        }
 
         // The shell script has to be made available on the final container(s)
         // where it will be executed.
@@ -85,9 +90,10 @@ public class YarnClientTester {
         // We do not need to set this as a local resource for the application
         // master as the application master does not need it.
         String hdfsShellScriptLocation = "";
-        String shellScriptPath = "";
         long hdfsShellScriptLen = 0;
         long hdfsShellScriptTimestamp = 0;
+        String shellScriptPath = "";
+        String SCRIPT_PATH = "";
         if (!shellScriptPath.isEmpty()) {
             Path shellSrc = new Path(shellScriptPath);
             String shellPathSuffix =
@@ -100,29 +106,30 @@ public class YarnClientTester {
             hdfsShellScriptLen = shellFileStatus.getLen();
             hdfsShellScriptTimestamp = shellFileStatus.getModificationTime();
         }
-
-        Map<String, String> shellCommand = new HashMap<>();
+        String shellCommand = "";
+        String shellCommandPath = "";
         if (!shellCommand.isEmpty()) {
-//            addToLocalResources(fs, null, shellCommandPath, appId.toString(),
-//                    localResources, shellCommand);
+            addToLocalResources(fs, null, shellCommandPath, appId.toString(),
+                    localResources, shellCommand);
         }
 
-        Object[] shellArgs = new Object[10];
+        String[] shellArgs = {};
+        String shellArgsPath = "";
         if (shellArgs.length > 0) {
-//            addToLocalResources(fs, null, shellArgsPath, appId.toString(),
-//                    localResources, StringUtils.join(shellArgs, " "));
+            addToLocalResources(fs, null, shellArgsPath, appId.toString(),
+                    localResources, StringUtils.join(shellArgs, " "));
         }
 
         // Set the env variables to be setup in the env where the application master will be run
-        System.out.println("Set the environment for the application master");
-        Map<String, String> env = new HashMap<String, String>();
+        log.info("Set the environment for the application master");
+        Map<String, String> env = new HashMap<>();
 
         // put location of shell script into env
         // using the env info, the application master will create the correct local resource for the
         // eventual containers that will be launched to execute the shell scripts
-//        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTLOCATION, hdfsShellScriptLocation);
-//        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTTIMESTAMP, Long.toString(hdfsShellScriptTimestamp));
-//        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTLEN, Long.toString(hdfsShellScriptLen));
+        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTLOCATION, hdfsShellScriptLocation);
+        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTTIMESTAMP, Long.toString(hdfsShellScriptTimestamp));
+        env.put(DSConstants.DISTRIBUTEDSHELLSCRIPTLEN, Long.toString(hdfsShellScriptLen));
 
         // Add AppMaster.jar location to classpath
         // At some point we should not be required to add
@@ -132,7 +139,7 @@ public class YarnClientTester {
         // the classpath to "." for the application jar
         StringBuilder classPathEnv = new StringBuilder(Environment.CLASSPATH.$$())
                 .append(ApplicationConstants.CLASS_PATH_SEPARATOR).append("./*");
-        for (String c : configuration.getStrings(
+        for (String c : conf.getStrings(
                 YarnConfiguration.YARN_APPLICATION_CLASSPATH,
                 YarnConfiguration.DEFAULT_YARN_CROSS_PLATFORM_APPLICATION_CLASSPATH)) {
             classPathEnv.append(ApplicationConstants.CLASS_PATH_SEPARATOR);
@@ -142,13 +149,13 @@ public class YarnClientTester {
                 "./log4j.properties");
 
         // Set the necessary command to execute the application master
-        Vector<CharSequence> vargs = new Vector<CharSequence>(30);
+        Set<CharSequence> vargs = new HashSet<>(30);
 
         // Set java executable command
-        System.out.println("Setting up app master command");
+        log.info("Setting up app master command");
         vargs.add(Environment.JAVA_HOME.$$() + "/bin/java");
         // Set Xmx based on am memory size
-        int amMemory = 0;
+        int amMemory = 1;
         vargs.add("-Xmx" + amMemory + "m");
         // Set class name
         String appMasterMainClass = "";
@@ -158,14 +165,18 @@ public class YarnClientTester {
         int containerVirtualCores = 1;
         int numContainers = 1;
         int shellCmdPriority = 1;
-        vargs.add("--container_memory " + containerMemory);
-        vargs.add("--container_vcores " + containerVirtualCores);
-        vargs.add("--num_containers " + numContainers);
-        vargs.add("--priority " + shellCmdPriority);
+        vargs.add("--container_memory " + String.valueOf(containerMemory));
+        vargs.add("--container_vcores " + String.valueOf(containerVirtualCores));
+        vargs.add("--num_containers " + String.valueOf(numContainers));
+        vargs.add("--priority " + String.valueOf(shellCmdPriority));
 
         Map<String, String> shellEnv = new HashMap<>();
         for (Map.Entry<String, String> entry : shellEnv.entrySet()) {
             vargs.add("--shell_env " + entry.getKey() + "=" + entry.getValue());
+        }
+        boolean debugFlag = true;
+        if (debugFlag) {
+            vargs.add("--debug");
         }
 
         vargs.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/AppMaster.stdout");
@@ -177,21 +188,20 @@ public class YarnClientTester {
             command.append(str).append(" ");
         }
 
-        System.out.println("Completed setting up app master command " + command.toString());
+        log.info("Completed setting up app master command " + command.toString());
         List<String> commands = new ArrayList<String>();
         commands.add(command.toString());
 
         // Set up the container launch context for the application master
-        Map<String, LocalResource> localResources = new HashMap<>();
         ContainerLaunchContext amContainer = ContainerLaunchContext.newInstance(
                 localResources, env, commands, null, null, null);
 
         // Set up resource type requirements
         // For now, both memory and vcores are supported, so we set memory and
         // vcores requirements
-        int amVCores = 0;
+        int amVCores = 1;
         Resource capability = Resource.newInstance(amMemory, amVCores);
-        applicationSubmissionContext.setResource(capability);
+        appContext.setResource(capability);
 
         // Service data is a binary blob that can be passed to the application
         // Not needed in this scenario
@@ -201,7 +211,7 @@ public class YarnClientTester {
         if (UserGroupInformation.isSecurityEnabled()) {
             // Note: Credentials class is marked as LimitedPrivate for HDFS and MapReduce
             Credentials credentials = new Credentials();
-            String tokenRenewer = configuration.get(YarnConfiguration.RM_PRINCIPAL);
+            String tokenRenewer = conf.get(YarnConfiguration.RM_PRINCIPAL);
             if (tokenRenewer == null || tokenRenewer.length() == 0) {
                 throw new IOException(
                         "Can't get Master Kerberos principal for the RM to use as renewer");
@@ -212,7 +222,7 @@ public class YarnClientTester {
                     fs.addDelegationTokens(tokenRenewer, credentials);
             if (tokens != null) {
                 for (Token<?> token : tokens) {
-                    System.out.println("Got dt for " + fs.getUri() + "; " + token);
+                    log.info("Got dt for " + fs.getUri() + "; " + token);
                 }
             }
             DataOutputBuffer dob = new DataOutputBuffer();
@@ -220,8 +230,32 @@ public class YarnClientTester {
             ByteBuffer fsTokens = ByteBuffer.wrap(dob.getData(), 0, dob.getLength());
             amContainer.setTokens(fsTokens);
         }
+        appContext.setAMContainerSpec(amContainer);
 
-        applicationSubmissionContext.setAMContainerSpec(amContainer);
+
+        // Set the priority for the application master
+        int amPriority = 1;
+        Priority pri = Priority.newInstance(amPriority);
+        appContext.setPriority(pri);
+
+        // Set the queue to which this application is to be submitted in the RM
+        String amQueue = "default";
+        appContext.setQueue(amQueue);
+
+        // Submit the application to the applications manager
+        // SubmitApplicationResponse submitResp = applicationsManager.submitApplication(appRequest);
+        yarnClient.submitApplication(appContext);
+
+
+        // Get application report for the appId we are interested in
+        ApplicationReport report = yarnClient.getApplicationReport(appId);
+
+        //if one task taking too long,we can manually kill this task by this instruction
+        yarnClient.killApplication(appId);
     }
 
+    private static void addToLocalResources(FileSystem fs, String appMasterJar, String appMasterJarPath,
+            String toString, Map<String, LocalResource> localResources, Object o) {
+        //todo
+    }
 }
