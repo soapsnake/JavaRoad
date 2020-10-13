1735d92bc13c4260bc511ce229b95eb77d8e6a3f
soapsnake <soapsnake@gmail.com>
2020-10-13 23:40:16 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question316.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/str/Question316.java
@@ -8,6 +8,14 @@ public class Question316 {
 
     //去掉s中所有重复的字符
     public String removeDuplicateLetters(String s) {
-
+        int[] cnt = new int[26];
+        int pos = 0; // the position for the smallest s[i]
+        for (int i = 0; i < s.length(); i++)
+            cnt[s.charAt(i) - 'a']++;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) < s.charAt(pos)) pos = i;
+            if (--cnt[s.charAt(i) - 'a'] == 0) break;
+        }
+        return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
     }
 }
