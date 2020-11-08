791c11b66e4254f27d959a9f7e084a8bd8b42224
soapsnake <soapsnake@gmail.com>
2020-11-08 23:24:37 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/tree/binarytree/Question563.java
@@ -0,0 +1,25 @@
+package com.soapsnake.algorithms.leetcode.tree.binarytree;
+
+import com.soapsnake.algorithms.structures.tree.TreeNode;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-11-08
+ */
+public class Question563 {
+
+    //leetcode563
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return result;
+    }
+    int result = 0;
+    private int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+        result += Math.abs(left - right);
+        return left + right + root.val;
+    }
+}
