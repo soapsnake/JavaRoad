874459dc1cbf3faef4ae0e8a47d22a7327a49c38
liudun <soapsnake@gmail.com>
2020-11-10 00:26:15 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/tree/binarytree/Question1026.java
@@ -0,0 +1,22 @@
+package com.soapsnake.algorithms.leetcode.tree.binarytree;
+
+import com.soapsnake.algorithms.structures.tree.TreeNode;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-11-10
+ */
+public class Question1026 {
+
+    //leetcode1026
+    public int maxAncestorDiff(TreeNode root) {
+        return dfs(root, root.val, root.val);
+    }
+
+    public int dfs(TreeNode root, int min, int max) {
+        if (root == null) return max - min;
+        max = Math.max(max, root.val);
+        min = Math.min(min, root.val);
+        return Math.max(dfs(root.left, min, max), dfs(root.right, min, max));
+    }
+}
