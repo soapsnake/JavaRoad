5f576d3860c5c598c49f40b7115ee015a4f6d81a
liudun <soapsnake@gmail.com>
2020-11-15 21:13:38 +0800

    fix

---
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/matrix/Question458.java
@@ -0,0 +1,16 @@
+package com.soapsnake.algorithms.leetcode.matrix;
+
+/**
+ * @author liudun <liudun@kuaishou.com>
+ * Created on 2020-11-14
+ */
+public class Question458 {
+
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int pigs = 0;
+        while (Math.pow(minutesToTest / minutesToDie + 1, pigs) < buckets) {
+            pigs += 1;
+        }
+        return pigs;
+    }
+}
