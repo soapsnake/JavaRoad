5cc914b7523a538c58a7e55a9916bb1c36a8303f
liudun <liudun@kuaishou.com>
2021-03-04 14:38:06 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question645.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/leetcode/array/Question645.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
  */
 public class Question645 {
 
-    public static void main(String[] args) {
-        Question645 question645 = new Question645();
-        int[] nums = {1, 2, 2, 4};
-        System.out.println(Arrays.toString(question645.findErrorNums(nums)));
-    }
+//    public static void main(String[] args) {
+//        Question645 question645 = new Question645();
+//        int[] nums = {1, 2, 2, 4};
+//        System.out.println(Arrays.toString(question645.findErrorNums(nums)));
+//    }
 
     /**
      * Input: nums = [1,2,2,4]
@@ -46,4 +46,42 @@ public class Question645 {
         }
         return res;
     }
+
+    //leetcode268
+    public static int missingNumber(int[] nums) {
+        //思路:nums.length+1,算出总和,然后把nums总和加起来,算差值
+        int n = nums.length;
+        int should = n * (n + 1) / 2;
+        int real = 0;
+        for (int i : nums) {
+            real += i;
+        }
+        return should - real;
+    }
+
+    public static int missingNumber2(int[] nums) {
+        //异或运算: a ^ a = 0, a ^ !a = 1(注意a要转成二进制,每一位单独异或,结果再转成十进制)
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9,6,4,2,3,5,7,0,1};
+//        int[] nums = {0};
+//        System.out.println(missingNumber(nums));
+
+//        System.out.println(1 ^ 1);
+
+        int a = 1;
+        int b = 10;
+        int c = 3 ^ 5;
+        System.out.println(c);
+
+//        System.out.println(2 ^ 1);
+    }
+
 }
