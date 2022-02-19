4d248c22c26ba6ed87f382f4464ec58a59a321d1
liu.dun <liu.dun@alibaba-inc.com>
2022-02-19 11:13:22 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/cruel/AboutGraph.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/cruel/AboutGraph.java
@@ -309,4 +309,24 @@ public class AboutGraph {
         }
         return max + 1;
     }
+
+
+    public int maximumANDSum(int[] nums, int numSlots) {
+        int ans = 0;
+        int[] f = new int[1 << (numSlots * 2)];
+        for ( int i = 0; i < f.length; i++) {
+            int c = Integer.bitCount(i);
+            if (c >= nums.length) {
+                continue;
+            }
+            for (int j = 0; j < numSlots * 2; ++j) {
+                if ((i & (1 << j)) == 0) {
+                    int s = i | (1 << j);
+                    f[s] = Math.max(f[s], f[i] + ((j / 2 + 1) & nums[c]));
+                    ans = Math.max(ans, f[s]);
+                }
+            }
+        }
+        return ans;
+    }
 }
