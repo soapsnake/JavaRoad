ab47d92e1c05a00a5fb77abecdb861040942f5ed
liudun <soapsnake@gmail.com>
2022-06-22 12:38:58 +0800

    fix

--- algorithms/src/main/java/com/soapsnake/algorithms/cruel/WeeklyContext298.java
+++ algorithms/src/main/java/com/soapsnake/algorithms/cruel/WeeklyContext298.java
@@ -123,5 +123,34 @@ public class WeeklyContext298 {
         System.out.println(week.longestSubsequence(s, k));
     }
 
+
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            count = 0;
+            dfs(i, new boolean[n], bombs);
+            ans = Math.max(ans, count);
+        }
+        return ans;
+    }
+    int count = 0;
+    private void dfs(int idx, boolean[] v, int[][] bombs) {
+        count++;
+        v[idx] = true;
+        int n = bombs.length;
+        for (int i = 0; i < n; i++) {
+            if (!v[i] && inRange(bombs[idx], bombs[i])) {
+                v[i] = true;
+                dfs(i, v, bombs);
+            }
+        }
+    }
+
+    private boolean inRange(int[] a, int[] b) {
+        long dx = a[0] - b[0], dy = a[1] - b[1], r = a[2];
+        return dx * dx + dy * dy <= r * r;
+    }
+
     
 }
